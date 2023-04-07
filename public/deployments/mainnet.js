23d394952c1e7265a4c173e71427c3a1883b9253
Johnny Gault <johnnygault@protonmail.com>
2023-04-07 12:41:21 -0700

    Add mainnet  🎉

---
+++ public/deployments/mainnet.js
@@ -0,0 +1,26 @@
+const deployment = {
+  avaURL: "https://api.avax.network",
+  ethURL: "https://api.avax.network/ext/bc/C/rpc",
+  orcURL: "https://orchestrator-mainnet.fly.dev",
+  pExplorerURL: "https://api.avax.network",
+  cExplorerURL: "https://api.avax.network",
+  chain: {
+    name: "mainnet",
+    chainId: 43114,
+  },
+  storage: "0x1cEa17F9dE4De28FeB6A102988E12D4B90DfF1a9",
+  multicall3: "0xcA11bde05977b3631167028862bE2a173976CA11",
+  EOALabels: {
+    "0xf5c149aCB200f5BC8FC5e51dF4a7DEf38d64cfB2": "deployer",
+  },
+  contracts: {}, // will merge in contracts.json
+  dashboard: [], // will merge in dashboard.json
+};
+
+async function deploymentFn() {
+  deployment.contracts = await fetch("/deployments/contracts.json").then((res) => res.json());
+  deployment.dashboard = await fetch("/deployments/dashboard.json").then((res) => res.json());
+  return deployment;
+}
+
+export { deploymentFn };
--- public/deployments/selected.js
+++ public/deployments/selected.js
@@ -3,6 +3,7 @@ import { deploymentFn as deploymentFnAnrLocal } from "./anr-local.js";
 import { deploymentFn as deploymentFnHardhat } from "./hardhat.js";
 import { deploymentFn as deploymentFnFuji } from "./fuji.js";
 import { deploymentFn as deploymentFnMainnetTest } from "./mainnet-test.js";
+import { deploymentFn as deploymentFnMainnet } from "./mainnet.js";
 
 // Return whichever deployment is selected in localStorage
 
@@ -12,6 +13,9 @@ async function init() {
   if (!localStorage.getItem("deployment")) localStorage.setItem("deployment", "anr-fly");
 
   switch (localStorage.getItem("deployment")) {
+    case "mainnet":
+      DEPLOYMENT = await deploymentFnMainnet();
+      break;
     case "anr-fly":
       DEPLOYMENT = await deploymentFnAnrFly();
       break;

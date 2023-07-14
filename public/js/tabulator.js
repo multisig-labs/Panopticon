767b7d3be8159ebf98059f4da37adebf4a94ce4e
Johnny Gault <john@multisiglabs.org>
2023-07-13 17:44:15 -0700

    Add header count for stakers

--- public/js/tabulator.js
+++ public/js/tabulator.js
@@ -241,6 +241,17 @@ const stakersDef = {
   selectable: true,
   clipboard: "copy",
   clipboardCopyRowRange: "selected",
+  groupBy: function (data) {
+    return "Eligible NodeOps";
+  },
+  groupHeader: function (value, count, data, group) {
+    //value - the value all members of this group share
+    //count - the number of rows in this group
+    //data - an array of all the row data objects in this group
+    //group - the group component for the group
+
+    return value + "<span style='color:#d00; margin-left:10px;'>(" + count + " item)</span>";
+  },
   // rowFormatter: function (row) {
   //   let data = row.getData();
   //   if (data.ggpSlashAmt > 0) {
